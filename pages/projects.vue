c65d199cb7d8107368e0b6c770076494e0040e1c
Alexander B <mail@xanderbarkhatov.com>
2023-05-31 17:03:48 +0600

    fix: remove project image

--- pages/projects.vue
+++ pages/projects.vue
@@ -4,22 +4,12 @@
 
     <div class="grid gap-8 sm:grid-cols-2">
       <NuxtLink to="https://t.me/cinesharebot" target="_blank">
-        <UiCard class="!space-y-0 !p-0">
-          <NuxtImg
-            src="/cineshare.jpg"
-            alt="Cinema clapperboard"
-            width="640"
-            height="640"
-            format="webp"
-          />
+        <UiCard>
+          <h3 class="text-lg font-semibold">CineShare</h3>
 
-          <div class="space-y-2 p-5">
-            <h3 class="text-lg font-semibold">CineShare</h3>
-
-            <div class="text-sm text-gray-300">
-              Find and share movies and TV shows in any Telegram chat, using
-              inline queries
-            </div>
+          <div class="text-sm text-gray-300">
+            Find and share movies and TV shows in any Telegram chat, using
+            inline queries
           </div>
         </UiCard>
       </NuxtLink>
