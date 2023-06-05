702526b0099cd8f3108e6da6317151959ba3312a
Alexander B <mail@xanderbarkhatov.com>
2023-06-05 23:46:55 +0600

    add project

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,15 +1,30 @@
+<script setup lang="ts">
+const projects = [
+  {
+    name: "CineShare",
+    description:
+      "Find and share movies and TV shows in any Telegram chat, using inline queries",
+    url: "https://t.me/cinesharebot",
+  },
+  {
+    name: "Tic-Tac-Toe",
+    description: "Simple Tic-Tac-Toe game with nice animations",
+    url: "https://tictactoe.xanderbarkhatov.com",
+  },
+];
+</script>
+
 <template>
   <main class="space-y-8">
     <h1 class="text-4xl font-bold">Projects</h1>
 
     <div class="grid gap-8 sm:grid-cols-2">
-      <NuxtLink to="https://t.me/cinesharebot" target="_blank">
+      <NuxtLink v-for="p in projects" :key="p.name" :to="p.url" target="_blank">
         <UiCard>
-          <h3 class="text-lg font-semibold">CineShare</h3>
+          <h3 class="text-lg font-semibold">{{ p.name }}</h3>
 
           <div class="text-sm text-gray-300">
-            Find and share movies and TV shows in any Telegram chat, using
-            inline queries
+            {{ p.description }}
           </div>
         </UiCard>
       </NuxtLink>
