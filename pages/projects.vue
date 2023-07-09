5a685807e087e493e48bb85e5473cfb9b61b282b
Alexander B <mail@xanderbarkhatov.com>
2023-07-09 18:25:18 +0600

    add project

--- pages/projects.vue
+++ pages/projects.vue
@@ -7,8 +7,13 @@ const projects = [
     url: "https://t.me/cinesharebot",
   },
   {
+    name: "Emoji Search",
+    description: "AI-powered emoji search",
+    url: "https://emojisearch.xanderbarkhatov.com/",
+  },
+  {
     name: "Tic-Tac-Toe",
-    description: "Simple Tic-Tac-Toe game with nice animations",
+    description: "Tic-Tac-Toe with nice animations",
     url: "https://tictactoe.xanderbarkhatov.com",
   },
 ];
@@ -16,16 +21,18 @@ const projects = [
 
 <template>
   <PageContainer title="Projects">
-    <div class="grid gap-8 sm:grid-cols-2">
-      <NuxtLink v-for="p in projects" :key="p.name" :to="p.url" target="_blank">
-        <UiCard>
-          <h3 class="text-lg font-semibold">{{ p.name }}</h3>
+    <div class="grid gap-4 sm:grid-cols-2">
+      <div v-for="half in halfArray(projects)" class="flex flex-col gap-4">
+        <NuxtLink v-for="p in half" :key="p.name" :to="p.url" target="_blank">
+          <UiCard>
+            <h3 class="text-lg font-semibold">{{ p.name }}</h3>
 
-          <div class="text-sm text-gray-300">
-            {{ p.description }}
-          </div>
-        </UiCard>
-      </NuxtLink>
+            <div class="text-sm text-gray-300">
+              {{ p.description }}
+            </div>
+          </UiCard>
+        </NuxtLink>
+      </div>
     </div>
   </PageContainer>
 </template>
