be9f0bb45f4989c73360cdc962e948f0bee49b66
Alexander Barkhatov <mail@xanderbarkhatov.com>
2023-11-15 21:25:31 +0100

    build: vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,31 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import { FontaineTransform } from "fontaine";
-
+import vercel from "@astrojs/vercel/static";
 import qwikdev from "@qwikdev/astro";
+import { FontaineTransform } from "fontaine";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
+  adapter: vercel({
+    webAnalytics: true,
+    imageService: true,
+    imagesConfig: {
+      sizes: [318, 636],
+    },
+  }),
   integrations: [tailwind(), qwikdev()],
   vite: {
-    plugins: [FontaineTransform.vite({
-      fallbacks: ["BlinkMacSystemFont", "Segoe UI", "Helvetica Neue", "Arial", "Noto Sans"]
-    })]
-  }
+    plugins: [
+      FontaineTransform.vite({
+        fallbacks: [
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Helvetica Neue",
+          "Arial",
+          "Noto Sans",
+        ],
+      }),
+    ],
+  },
 });
