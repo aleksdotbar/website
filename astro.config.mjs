6e7fa739c923faa7fd97d3544d10bf7d7f398055
Alexander Barkhatov <mail@xanderbarkhatov.com>
2023-11-15 23:05:09 +0100

    build: remove vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/static";
 import { FontaineTransform } from "fontaine";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
-  adapter: vercel({
-    webAnalytics: true,
-  }),
   integrations: [tailwind()],
   vite: {
     plugins: [
