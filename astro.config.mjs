35db3ab9d75fd994e4ecc83cbc0c18735ef10c4b
Alexander Barkhatov <mail@xanderbarkhatov.com>
2023-12-16 15:28:36 +0100

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
-import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/static";
 import qwikdev from "@qwikdev/astro";
+import { defineConfig } from "astro/config";
 import { FontaineTransform } from "fontaine";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
+  adapter: vercel({
+    imageService: true,
+  }),
   integrations: [tailwind(), qwikdev()],
   vite: {
     plugins: [
