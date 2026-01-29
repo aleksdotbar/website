df9adf87dad2b443e0b54a3f8936debe13749897
Aleksandr B. <pm@aleks.bar>
2026-01-29 16:59:00 +0100

    cf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,16 @@
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/static";
 import qwikdev from "@qwikdev/astro";
 import { defineConfig } from "astro/config";
 import { FontaineTransform } from "fontaine";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  adapter: vercel({
-    imageService: true,
-    imagesConfig: {
-      domains: [],
-      sizes: [590],
-    },
-  }),
+
   integrations: [tailwind(), qwikdev()],
+
   vite: {
     plugins: [
       FontaineTransform.vite({
